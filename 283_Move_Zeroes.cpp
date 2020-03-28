3d1379d421fbc12ac5652321031d63c1a3dfca86
yufansong <yufansong@foxmail.com>
2020-03-28 21:11:16 +0800

    283

---
+++ 283_Move_Zeroes.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+// class Solution {
+// public:
+//     void moveZeroes(vector<int>& nums) {
+       
+//         vector<int> nonZerolements;
+
+//         for(int i =0; i<nums.size();i++)
+//             if (nums[i])
+//                 nonZerolements
+//     }
+// };
+
+int main(){
+    printf("test");
+    return 0;
+}
