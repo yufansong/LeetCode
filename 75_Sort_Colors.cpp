6b8bb84603439f757386e3d82861f68a52841577
yufansong <yufansong@foxmail.com>
2020-04-04 20:55:28 +0800

    72 sort colors

---
+++ 75_Sort_Colors.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <cassert>
+using namespace std;
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int zero = -1;
+        int two = nums.size();
+        for(int i =0;i<two;)
+        {
+            if (nums[i] == 1)
+                i++;
+            else if (nums[i]==2)
+                swap(nums[i],nums[--two]);
+            else
+                swap(nums[++zero],nums[i++]);
+        }
+    }
+};
