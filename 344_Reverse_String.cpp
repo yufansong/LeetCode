434d58cfa0e94b06c9e282342be49d805ebfa09f
yufansong <yufansong@foxmail.com>
2020-04-05 12:30:22 +0800

    344 reverse string

---
+++ 344_Reverse_String.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int i =0, j=s.size()-1;
+        for(;i<j;i++,j--){
+            swap(s[i],s[j]);
+        }
+    }
+};
