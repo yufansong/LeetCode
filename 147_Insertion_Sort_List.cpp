58bef50794db62723a950436a62b81326d4ff034
yufansong <yufansong@foxmail.com>
2020-04-14 00:35:51 +0800

    147

---
+++ 147_Insertion_Sort_List.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+        ListNode * dummyhead = new ListNode(0);
+        // dummyhead->next = head;
+        ListNode * p = head;
+        while(p != NULL){
+            ListNode *next = p->next;
+            ListNode *cur = dummyhead;
+            while(cur->next && cur->val < p->val)
+                cur = cur->next;
+            p->next = cur->next;
+            cur->next = p;
+            p = next;
+        }
+        ListNode *retNode = dummyhead->next;
+        delete dummyhead;
+        return retNode;
+    }
+};
