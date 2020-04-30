1d7d1ab8f7351314e7f75443d59799708d22388f
yufansong <yufansong@foxmail.com>
2020-04-30 22:55:19 +0800

    238 product of array except self

--- 30_challenge/238_Product_of_Array_Except_Self.cpp
+++ 30_challenge/238_Product_of_Array_Except_Self.cpp
@@ -5,6 +5,15 @@ using namespace std;
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-                
+         vector<int> result(nums.size(),1);
+         for(int i=0;i<nums.size()-1;i++){
+             result[i+1] = result[i] * nums[i];
+         }
+         int right = 1;
+         for(int i=nums.size()-1;i>=0;i--){
+             result[i] *= right;
+             right *= nums[i];
+         }
+         return result;
     }
 };
