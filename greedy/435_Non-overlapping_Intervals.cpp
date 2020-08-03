cc37c1ac4ca9db7ea216adf0a5d0a8e44c5becf0
yufansong <yufansong@foxmail.com>
2020-08-03 08:48:19 +0800

    222 count complete tree nodes

--- greedy/435_Non-overlapping_Intervals.cpp
+++ greedy/435_Non-overlapping_Intervals.cpp
@@ -7,6 +7,8 @@ public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
         int res=0, last = 0;
         sort(intervals.begin(),intervals.end());
+        // for(int i=0;i<intervals.size();i++)
+        //     cout<<intervals[i][0]<<intervals[i][1]<<endl;
         for(int i=1;i<intervals.size();i++){
             if(intervals[i][0] < intervals[last][1]){
                 res++;
