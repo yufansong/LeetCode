94e7d150f3b423715a05a32fc5fca5c80443a15a
yufansong <yufansong@foxmail.com>
2020-08-03 15:43:35 +0800

    list quick sort

--- Link/148_Sort_List.cpp
+++ Link/148_Sort_List.cpp
@@ -56,3 +56,46 @@ private:
         return retNode;
     }
 };
+
+
+// Quick Sort Link
+// struct Node 
+// {
+// 	int key;
+// 	Node* next;
+// 	Node(int nKey, Node* pNext)
+// 		: key(nKey)
+// 		, next(pNext)
+// 	{}
+// };
+ 
+ 
+// Node* GetPartion(Node* pBegin, Node* pEnd)
+// {
+// 	int key = pBegin->key;
+// 	Node* p = pBegin;
+// 	Node* q = p->next;
+ 
+// 	while(q != pEnd)
+// 	{
+// 		if(q->key < key)
+// 		{
+// 			p = p->next;
+// 			swap(p->key,q->key);
+// 		}
+ 
+// 		q = q->next;
+// 	}
+// 	swap(p->key,pBegin->key);
+// 	return p;
+// }
+ 
+// void QuickSort(Node* pBeign, Node* pEnd)
+// {
+// 	if(pBeign != pEnd)
+// 	{
+// 		Node* partion = GetPartion(pBeign,pEnd);
+// 		QuickSort(pBeign,partion);
+// 		QuickSort(partion->next,pEnd);
+// 	}
+// }
